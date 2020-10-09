81b79717254c34e0995f2f250e56189f3082cd6e
Bhavish <hakbhavish@gmail.com>
2020-10-09 19:37:18 +0530

    updated job schema

--- server/src/models/job.ts
+++ server/src/models/job.ts
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
 
 const schema = mongoose.Schema({
-  title: String,
+  jobTitle: String,
   description: String,
+  salaryStart: String,
+  salaryEnd: String,
+  jobForm: String,
+  currency: String,
+  location: String,
+  shiftHours: String,
+  shiftSchedule: String,
+  jobType: String,
+  ratio: String,
+  degreeRequirement: String,
+  workExperience: String,
 });
 
 const JobSchema = mongoose.model("Post", schema);
